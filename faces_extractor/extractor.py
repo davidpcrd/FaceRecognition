7df75e2cb7548e7565fefb240644e2fa841dc7a4
davidpcrd <32419820+Oxmas@users.noreply.github.com>
2022-01-20 17:59:33 +0100

    some change

--- faces_extractor/extractor.py
+++ faces_extractor/extractor.py
@@ -4,30 +4,46 @@ import os
 from tqdm import tqdm
 import glob
 import cv2
-from functions import process,process_dpl
-from mtcnn import MTCNN
+
+def get_relative_files_from_py(py_file, filename):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
 
 parser = argparse.ArgumentParser()
  
 parser.add_argument("-i", "--input", help = "Input folder. default=../images/before_process", default="../images/before_process")
 parser.add_argument("-o", "--output", help = "image output folder. default=../images/face_extract", default="../images/faces_extract")
 parser.add_argument("-t", "--target-size", help = "output target size. default=150,150", default="150,150")
+parser.add_argument("-d", "--dpl", help = "Use dpl face detection", action="store_true")
+parser.add_argument("-r", "--relative", help = "Use relative file from .py", action="store_true")
 
 args = vars(parser.parse_args())
 input_folder = args["input"]
 output_folder = args["output"]
+dpl = args["dpl"]
 target_size = tuple([int(x) for x in args["target_size"].split(',')])
 
-os.chdir(os.path.dirname(os.path.realpath(__file__))) ## SET WORKING DIR TO faces_scraper
+if args["relative"]:
+    os.chdir(os.path.dirname(os.path.realpath(__file__))) ## SET WORKING DIR TO faces_scraper
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+
 
 # Load opencv/MTCNN
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-detector = MTCNN()
+if not dpl:
+    from functions import process
+    face_cascade = cv2.CascadeClassifier(get_relative_files_from_py(__file__, "haarcascade_frontalface_default.xml"))
+else:
+    from functions import process_dpl
+    from mtcnn import MTCNN
+    detector = MTCNN()
 
 # Obtient toutes les photos
 files = glob.glob(os.path.join(input_folder,"*"))
 
 for f in tqdm(files):
     filename = os.path.basename(f).replace("scrape","face")
-    # process(f,os.path.join(output_folder, filename), target_size, face_cascade)
-    process_dpl(f,os.path.join(output_folder, filename), target_size, detector)
+    if not dpl:
+        process(f,os.path.join(output_folder, filename), target_size, face_cascade)
+    else:
+        process_dpl(f,os.path.join(output_folder, filename), target_size, detector)
