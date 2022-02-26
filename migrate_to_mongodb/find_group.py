0810d4e2ce9ab4671b31a737ae063dadefbd0df1
davidpcrd <32419820+Oxmas@users.noreply.github.com>
2022-02-26 13:49:25 +0100

    move files

--- migrate_to_mongodb/find_group.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from pprint import pprint
-
-client = MongoClient("mongodb://root:root@localhost:27017/")
-db=client.celebrities
-
-a = db.faces.find({"group_id":5})
-for _ in a:
-    pprint(_)
