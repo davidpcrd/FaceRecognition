58c0a534f9bd7cc06f25760d8c36e7c65f653008
davidpcrd <32419820+Oxmas@users.noreply.github.com>
2022-03-12 14:10:40 +0100

    delete file with old db structure

--- transfer_test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ">>> Start scraping celebrities faces"
-python celebrities\celebrities_scraper.py -o ../images\celebrities_before_newdb
-echo ">>> Encode it"
-python encoding.py -i images\celebrities_before_newdb -t celebrities_faces -u
-echo ">>> Add group_id"
-python generate_group_id.py -t celebrities_faces
